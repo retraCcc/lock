4b3ff16b31077ff8a1b8e0055a2e0e507de67d91
retraCcc <100065385+retraCcc@users.noreply.github.com>
2022-03-12 18:05:54 -0500

    Create dhlock.lua

---
+++ dhlock.lua
@@ -0,0 +1,136 @@
+local target = true
+local key = Enum.KeyCode.Q
+local pred = 0.131
+local chat = false
+local notifications = true
+local partmode = true
+local airfunc = true
+local partz = "UpperTorso"
+
+------------------------
+
+------------------------
+
+------------------------
+
+------------------------
+
+------------------------
+
+    local types = {
+        Ball = Enum.PartType.Ball,
+        Block = Enum.PartType.Block, 
+        Cylinder = Enum.PartType.Cylinder
+    }
+    
+    local Tracer = Instance.new("Part", game.Workspace)
+    Tracer.Name = "paigeisbest"	
+    Tracer.Anchored = true		
+    Tracer.CanCollide = false
+    Tracer.Transparency = 0.65
+    Tracer.Parent = game.Workspace	
+    Tracer.Shape = types.Block
+    Tracer.Size = Vector3.new(8,8,8)
+    Tracer.Color = Color3.fromRGB(222, 222, 222)
+    
+local plr = game.Players.LocalPlayer
+local mouse = plr:GetMouse()
+local Runserv = game:GetService("RunService")
+
+circle = Drawing.new("Circle")
+circle.Color = Color3.fromRGB(255,255,255)
+circle.Thickness = 0
+circle.NumSides = 732
+circle.Radius = 120
+circle.Thickness = 0
+circle.Transparency = 0
+circle.Visible = true
+circle.Filled = false
+
+Runserv.RenderStepped:Connect(function()
+    circle.Position = Vector2.new(mouse.X,mouse.Y+35)
+end)
+    
+    	local guimain = Instance.new("Folder", game.CoreGui)
+    	local CC = game:GetService"Workspace".CurrentCamera
+    local LocalMouse = game.Players.LocalPlayer:GetMouse()
+    	local Locking = false
+    
+    	function getClosestPlayerToCursor()
+		local closestPlayer
+		local shortestDistance = circle.Radius
+
+		for i, v in pairs(game.Players:GetPlayers()) do
+			if v ~= game.Players.LocalPlayer and v.Character and v.Character:FindFirstChild("Humanoid") and v.Character.Humanoid.Health ~= 0 and v.Character:FindFirstChild("LowerTorso") then
+				local pos = CC:WorldToViewportPoint(v.Character.PrimaryPart.Position)
+				local magnitude = (Vector2.new(pos.X, pos.Y) - Vector2.new(LocalMouse.X, LocalMouse.Y)).magnitude
+				if magnitude < shortestDistance then
+					closestPlayer = v
+					shortestDistance = magnitude
+				end
+			end
+		end
+		return closestPlayer
+	end
+    
+        local UserInputService = game:GetService("UserInputService")
+
+    UserInputService.InputBegan:Connect(function(keygo,ok)
+           if (not ok) then
+           if (keygo.KeyCode == key) then
+               if target == true then
+               Locking = not Locking
+                                   
+               if Locking then
+        Plr = getClosestPlayerToCursor()
+                if chat then
+        local A_1 = "local a2"..tostring(Plr.Character.Humanoid.DisplayName) local A_2 = "All" local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest Event:FireServer(A_1, A_2) 
+        	end	
+               if notifications then
+    end
+    elseif not Locking then
+         if chat then
+        local A_1 = "local a1" local A_2 = "All" local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest Event:FireServer(A_1, A_2) 
+        	end	
+           elseif target == false then
+               
+               end          
+    end
+     
+	
+--
+if partmode then
+	game:GetService"RunService".Stepped:connect(function()
+		if Locking and Plr.Character and Plr.Character:FindFirstChild("LowerTorso") then
+			Tracer.CFrame = CFrame.new(Plr.Character.LowerTorso.Position+(Plr.Character.LowerTorso.Velocity * pred))
+		else
+			Tracer.CFrame = CFrame.new(0, 9999, 0)
+
+		end
+	end)
+end
+
+    
+    
+    --
+	local rawmetatable = getrawmetatable(game)
+	local old = rawmetatable.__namecall
+	setreadonly(rawmetatable, false)
+	rawmetatable.__namecall = newcclosure(function(...)
+		local args = {...}
+		if Locking and getnamecallmethod() == "FireServer" and args[2] == "UpdateMousePos" then
+			args[3] = Plr.Character[partz].Position+(Plr.Character[partz].Velocity * pred)
+			return old(unpack(args))
+		end
+		return old(...)
+	end)
+	if airfunc then
+	if Plr.Character.Humanoid.Jump == true and Plr.Character.Humanoid.FloorMaterial == Enum.Material.Air then
+        partz = "RightFoot"
+    else
+        partz = "LowerTorso"
+	end
+	end
+end
+end
+end)
